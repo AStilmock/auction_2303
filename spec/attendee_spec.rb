2940df1b1eb40cef9fec9bd175625bf17996ed48
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-04-26 09:12:54 -0600

    Feat: attendee class init and spec testing complete

---
+++ spec/attendee_spec.rb
@@ -0,0 +1,20 @@
+require 'rspec'
+require './lib/item'
+require './lib/attendee'
+
+RSpec.describe Attendee do
+  before(:each) do
+    @attendee1 = Attendee.new({name: 'Megan', budget: '$50'})
+  end
+
+  describe '#initialize' do
+    it 'exists' do
+      expect(@attendee1).to be_a(Attendee)
+    end
+
+    it 'has attenddee attributes' do
+      expect(@attendee1.name).to eq('Megan')
+      expect(@attendee1.budget).to eq('$50')
+    end
+  end
+end
