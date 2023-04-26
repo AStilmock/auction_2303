7481aa1881ad07ff58e1277da887be3a55c4d556
Andrew Stilmock <andrew.stilmock@gmail.com>
2023-04-26 11:58:51 -0600

    Feat: add bidder_info method and spec testing

--- lib/auction.rb
+++ lib/auction.rb
@@ -39,4 +39,25 @@ class Auction
     end
     bidders.uniq
   end
+
+  def bidder_info
+    bidder_info = Hash.new()
+    items.each do |item|
+      item.bids.each do |bid|
+        bidder_info[bid[0]] = {}
+      end
+    end
+    bidder_info.each do |key, value|
+      items.each do |item|
+        value[:budget] = key.budget
+        if item.bids.keys.include?(key)
+          if value[:items] == nil
+            value[:items] = [item.name]
+          else
+            value[:items] += [item.name]
+          end
+        end
+      end
+    end
+  end
 end
